6ba2d1d4b1dd0fa86491a000ad8c14a43c2c9cd4
Andrei Leon Salas <andreileonsalas@gmail.com>
2022-03-10 23:57:10 -0600

    Conneciton to database

--- blitz.config.js
+++ blitz.config.js
@@ -4,8 +4,10 @@ const config = {
     sessionMiddleware({
       cookiePrefix: "makeMyCurriculum",
       isAuthorized: simpleRolesIsAuthorized,
+      secureCookies: false, //necesario hasta que haya ssl en el proyecto
     }),
   ],
+
   /* Uncomment this to customize the webpack config
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
