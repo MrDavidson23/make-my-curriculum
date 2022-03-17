28f00c89ffd353103030dbb4c5948953fa0729ba
Andrei Leon Salas <andreileonsalas@gmail.com>
2022-03-17 00:01:30 -0600

    arreglar errores

--- app/core/components/LabeledSelect.js
+++ app/core/components/LabeledSelect.js
@@ -1,9 +1,8 @@
-import { forwardRef,useState } from "react"
+import { forwardRef, useState } from "react"
 import { useFormContext } from "react-hook-form"
-import { InputLabel, Select, MenuItem } from "@mui/material"
+import { InputLabel, Select, MenuItem, Typography } from "@mui/material"
 
 export const LabeledSelect = forwardRef(
-
   ({ label, outerProps, labelProps, name, ...props }, ref) => {
     const {
       register,
@@ -12,28 +11,33 @@ export const LabeledSelect = forwardRef(
     const error = Array.isArray(errors[name])
       ? errors[name].join(", ")
       : errors[name]?.message || errors[name]
-    
-    const [selected, setSelected] = useState((props.options.length === 0 ? "" : props.options[0].value))
+
+    const [selected, setSelected] = useState(
+      props.options.length === 0 ? "" : props.options[0].value
+    )
 
     return (
       <div {...outerProps}>
-          <Typography variant="body1" gutterBottom>
-            {label}
-          </Typography>
-          <InputLabel id={name+"-label"}>{props.label}</InputLabel>
-          <Select
-            label={props.label}
-            labelId={name+"-label"}
-            disabled={isSubmitting}
-            {...register(name)}
-            value={selected}
-            onChange={(event)=>{
-              setSelected(event.target.value)
-            }}
-          >
-          {props.options.map(
-              item => <MenuItem key={item.value} value={item.value}>{item.item}</MenuItem>)} 
-          </Select>
+        <Typography variant="body1" gutterBottom>
+          {label}
+        </Typography>
+        <InputLabel id={name + "-label"}>{props.label}</InputLabel>
+        <Select
+          label={props.label}
+          labelId={name + "-label"}
+          disabled={isSubmitting}
+          {...register(name)}
+          value={selected}
+          onChange={(event) => {
+            setSelected(event.target.value)
+          }}
+        >
+          {props.options.map((item) => (
+            <MenuItem key={item.value} value={item.value}>
+              {item.item}
+            </MenuItem>
+          ))}
+        </Select>
 
         {error && (
           <div
