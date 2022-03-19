0bb50ffec3fcf9bb9f83297bbb2e40e99fec604f
Andrei Leon Salas <andreileonsalas@gmail.com>
2022-03-18 22:11:05 -0600

    probando css

--- app/publications/components/PublicationForm.js
+++ app/publications/components/PublicationForm.js
@@ -2,31 +2,40 @@ import { Form } from "app/core/components/Form"
 import { LabeledTextField } from "app/core/components/LabeledTextField"
 import { LabeledSelect } from "app/core/components/LabeledSelect"
 
-import { TextField, Grid } from "@mui/material";
+import { TextField, Grid } from "@mui/material"
 
 export { FORM_ERROR } from "app/core/components/Form"
 
-const options = [{value:"Videojuego",item:"Videojuego"},{value:"Artículo",item:"Artículo"}]
+const options = [
+  { value: "Videojuego", item: "Videojuego" },
+  { value: "Artículo", item: "Artículo" },
+]
 
 export function PublicationForm(props) {
   return (
     <Form {...props}>
-        <div>
-        <Grid container 
-          direction="row"
-          spacing={2}>
-        <Grid item xs={6}>   
-        <LabeledTextField name="name" label="Nombre" placeholder="Nombre" />
-        <LabeledTextField name="date" label="Fecha de Publicación" placeholder="Fecha de Publicación" />
+      <div>
+        <Grid container direction="row" spacing={2}>
+          <Grid item xs={12}>
+            <LabeledTextField name="name" label="Nombre" placeholder="Nombre" />
+          </Grid>
+          <Grid item xs={12}>
+            <LabeledTextField
+              name="date"
+              label="Fecha de Publicación"
+              placeholder="Fecha de Publicación"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <LabeledTextField name="location" label="Dirección" placeholder="Dirección" />
+          </Grid>
+          <Grid item xs={12}>
+            <LabeledTextField name="institution" label="Institución" placeholder="Institución" />
+          </Grid>
+          <Grid item xs={12}>
+            <LabeledSelect name="tag" label="Tipo de Publicación" options={options} />
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-        <LabeledTextField name="location" label="Dirección" placeholder="Dirección" />
-        <LabeledTextField name="institution" label="Institución" placeholder="Institución" />
-        </Grid>
-        <Grid item xs={6}>   
-        <LabeledSelect name="tag" label="Tipo de Publicación"  options={options}/>
-        </Grid>
-      </Grid>
       </div>
     </Form>
   )
