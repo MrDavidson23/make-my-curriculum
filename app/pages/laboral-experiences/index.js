533312212963148f1e9b4278da05a8c62d9ed060
Lucas <66132859+SilverSoul16@users.noreply.github.com>
2022-03-28 18:56:09 -0600

    laboral experiences CRUD

--- app/pages/laboral-experiences/index.js
+++ app/pages/laboral-experiences/index.js
@@ -2,11 +2,18 @@ import { Suspense } from "react"
 import { Head, Link, usePaginatedQuery, useRouter, Routes } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import getLaboralExperiences from "app/laboral-experiences/queries/getLaboralExperiences"
+import deleteLaboralExperience from "app/laboral-experiences/mutations/deleteLaboralExperience"
+import InformationCard from "app/core/components/InformationCard"
+import { Grid } from "@mui/material"
 const ITEMS_PER_PAGE = 100
 export const LaboralExperiencesList = () => {
   const router = useRouter()
   const page = Number(router.query.page) || 0
+  const [deleteLaboralExperienceMutation] = useMutation(deleteLaboralExperience)
   const [{ laboralExperiences, hasMore }] = usePaginatedQuery(getLaboralExperiences, {
+    where: {
+      curriculumId: parseInt(props.curriculumId),
+    },
     orderBy: {
       id: "asc",
     },
@@ -30,26 +37,54 @@ export const LaboralExperiencesList = () => {
 
   return (
     <div>
-      <ul>
+      <Grid
+        container
+        direction="row"
+        spacing={2}
+        textAlign={"center"}
+        justify={"center"}
+        sx={{ mx: "auto", width: "100%" }}
+      >
         {laboralExperiences.map((laboralExperience) => (
-          <li key={laboralExperience.id}>
-            <Link
-              href={Routes.ShowLaboralExperiencePage({
-                laboralExperienceId: laboralExperience.id,
-              })}
-            >
-              <a>{laboralExperience.name}</a>
-            </Link>
-          </li>
+          <Grid item key={laboralExperience.id}>
+            <InformationCard
+              title={laboralExperience.position}
+              subtitle={laboralExperience.institution}
+              firstText={laboralExperience.location}
+              secondText={
+                laboralExperience.startYear.toLocaleDateString() +
+                "  -  " +
+                laboralExperience.finishYear.toLocaleDateString()
+              }
+              thirdText={laboralExperience.startMonth + " - " + laboralExperience.finishMonth}
+              handleOnEdit={() => {
+                router.push(
+                  Routes.EditLaboralExperiencePage({ laboralExperienceId: laboralExperience.id })
+                )
+              }}
+              handleOnDelete={async () => {
+                if (window.confirm("This will be deleted")) {
+                  await deleteLaboralExperienceMutation({
+                    id: laboralExperience.id,
+                  })
+                  //this.forceUpdate()
+                  router.push(
+                    Routes.EditCurriculumPage({ curriculumId: laboralExperience.curriculumId })
+                  )
+                }
+              }}
+            />
+          </Grid>
         ))}
-      </ul>
-
-      <button disabled={page === 0} onClick={goToPreviousPage}>
-        Previous
-      </button>
-      <button disabled={!hasMore} onClick={goToNextPage}>
-        Next
-      </button>
+        <Grid item xs={12} justify="center">
+          <button disabled={page === 0} onClick={goToPreviousPage}>
+            Previous
+          </button>
+          <button disabled={!hasMore} onClick={goToNextPage}>
+            Next
+          </button>
+        </Grid>
+      </Grid>
     </div>
   )
 }
@@ -63,13 +98,13 @@ const LaboralExperiencesPage = () => {
 
       <div>
         <p>
-          <Link href={Routes.NewLaboralExperiencePage()}>
-            <a>Create LaboralExperience</a>
+          <Link href={Routes.NewLaboralExperiencePage({ curriculumId: props.curriculumId })}>
+            <a>Create Laboral Experience</a>
           </Link>
         </p>
 
         <Suspense fallback={<div>Loading...</div>}>
-          <LaboralExperiencesList />
+          <LaboralExperiencesList curriculumId={props.curriculumId} />
         </Suspense>
       </div>
     </>
