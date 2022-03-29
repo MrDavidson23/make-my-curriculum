aca23d569f07e729e691372b033394026d95711e
Lucas <66132859+SilverSoul16@users.noreply.github.com>
2022-03-28 21:34:02 -0600

    Fix CurriculumID in CRUD

--- app/laboral-experiences/components/LaboralExperienceForm.js
+++ app/laboral-experiences/components/LaboralExperienceForm.js
@@ -1,10 +1,41 @@
 import { Form } from "app/core/components/Form"
 import { LabeledTextField } from "app/core/components/LabeledTextField"
+import { DatePickerField } from "app/core/components/DatePickerField"
+import { Grid } from "@mui/material"
+
 export { FORM_ERROR } from "app/core/components/Form"
 export function LaboralExperienceForm(props) {
   return (
     <Form {...props}>
-      <LabeledTextField name="name" label="Name" placeholder="Name" />
+      <div>
+        <Grid container direction="row" spacing={3}>
+          <Grid item xs={12}>
+            <LabeledTextField name="position" label="Position" placeholder="Position" />
+          </Grid>
+          <Grid item xs={12}>
+            <LabeledTextField name="location" label="Direction" placeholder="Direction" />
+          </Grid>
+          <Grid item xs={12}>
+            <LabeledTextField name="institution" label="Institution" placeholder="Institution" />
+          </Grid>
+          <Grid item xs={12}>
+            <DatePickerField name="startYear" label="Start Year" placeholder="Start Year" />
+          </Grid>
+          <Grid item xs={12}>
+            <DatePickerField
+              name="finishYear"
+              label="Completion Year"
+              placeholder="Completion Year"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <LabeledTextField name="startMonth" label="Start Month" placeholder="Start Month" />
+          </Grid>
+          <Grid item xs={12}>
+            <LabeledTextField name="finishMonth" label="Finish Month" placeholder="Finish Month" />
+          </Grid>
+        </Grid>
+      </div>
     </Form>
   )
 }
--- app/technical-educations/components/TechnicalEducationForm.js
+++ app/technical-educations/components/TechnicalEducationForm.js
@@ -1,10 +1,32 @@
 import { Form } from "app/core/components/Form"
 import { LabeledTextField } from "app/core/components/LabeledTextField"
+import { DatePickerField } from "app/core/components/DatePickerField"
+import { Grid } from "@mui/material"
+
 export { FORM_ERROR } from "app/core/components/Form"
 export function TechnicalEducationForm(props) {
   return (
     <Form {...props}>
-      <LabeledTextField name="name" label="Name" placeholder="Name" />
+      <div>
+        <Grid container direction="row" spacing={3}>
+          <Grid item xs={12}>
+            <LabeledTextField name="studies" label="Studies" placeholder="Studies" />
+          </Grid>
+          <Grid item xs={12}>
+            <LabeledTextField name="location" label="Location" placeholder="Location" />
+          </Grid>
+          <Grid item xs={12}>
+            <LabeledTextField name="institution" label="Institution" placeholder="Institution" />
+          </Grid>
+          <Grid item xs={12}>
+            <DatePickerField
+              name="completionYear"
+              label="Completion Year"
+              placeholder="Completion Year"
+            />
+          </Grid>
+        </Grid>
+      </div>
     </Form>
   )
 }
