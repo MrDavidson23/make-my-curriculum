4240f2e3c51f165d3f33106100245b296ba79237
Andrei Leon Salas <andreileonsalas@gmail.com>
2022-04-01 01:02:48 -0600

    fixed issues

--- app/curricula/components/structure/skills.js
+++ app/curricula/components/structure/skills.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import { Text, View, StyleSheet } from '@react-pdf/renderer';
+import React from "react"
+import { Text, View, StyleSheet } from "@react-pdf/renderer"
 
 let styles = StyleSheet.create({
-    title: {
-      fontSize: "14pt",
-      fontWeight: "bold",
-    }
-});
+  title: {
+    fontSize: "14pt",
+    fontWeight: "bold",
+  },
+})
 
 const Skills = (props) => {
-    styles = {...styles, ...props.styles}
-    return (
-        <>
-        { props.skills && (
-            <View>
-            <Text style={styles.title}>Habilidades</Text>
-                <View style={styles.text}>
-                {props.skills.map((skill) => (
-                    <Text>{skill.description}</Text>
-                ))}
-                </View>  
-            </View>  
-        )}
-        </>
-    )
+  styles = { ...styles, ...props.styles }
+  return (
+    <>
+      {props.skills && (
+        <View>
+          <Text style={styles.title}>Habilidades</Text>
+          <View style={styles.text}>
+            {props.skills.map((skill) => (
+              <Text key={skill.id}>{skill.description}</Text>
+            ))}
+          </View>
+        </View>
+      )}
+    </>
+  )
 }
 
 export default Skills
--- app/pages/curricula/[curriculumId]/pdf-view.js
+++ app/pages/curricula/[curriculumId]/pdf-view.js
@@ -3,8 +3,8 @@ import { Head, Link, useRouter, useQuery, useMutation, useParam, Routes, Router 
 import getCurriculum from "app/curricula/queries/getAllCurriculum"
 import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 import Layout from "app/core/layouts/Layout"
-import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
-import { PDFViewer } from '@react-pdf/renderer';
+import { Document, Page, Text, View, StyleSheet } from "@react-pdf/renderer"
+import { PDFViewer } from "@react-pdf/renderer"
 
 import Skills from "app/curricula/components/structure/skills"
 import Experiences from "app/curricula/components/structure/experience"
@@ -15,52 +15,52 @@ import References from "app/curricula/components/structure/references"
 
 // Create styles
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'row',
-      '@media max-width: 400': {
-        flexDirection: 'column',
-      },
-      margin: "2px"
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    "@media max-width: 400": {
+      flexDirection: "column",
     },
-    leftColumn: {
-      flexDirection: 'column',
-      width: 170,
-      paddingTop: 30,
-      paddingRight: 15,
-      paddingLeft: 15,
-      '@media max-width: 400': {
-        width: '100%',
-        paddingRight: 0,
-      },
-      '@media orientation: landscape': {
-        width: 200,
-      },
-      backgroundColor: '#3A298F',
-      color: '#FAF6F6'
+    margin: "2px",
+  },
+  leftColumn: {
+    flexDirection: "column",
+    width: 170,
+    paddingTop: 30,
+    paddingRight: 15,
+    paddingLeft: 15,
+    "@media max-width: 400": {
+      width: "100%",
+      paddingRight: 0,
     },
-    rightColumn: {
-      paddingTop: 30,
-      paddingRight: 15,
-      paddingLeft: 10,
-      '@media max-width: 400': {
-        width: '100%',
-        paddingRight: 0,
-      },
-      backgroundColor: '#FAF6F6',
+    "@media orientation: landscape": {
+      width: 200,
     },
-    title:{
-      fontSize: "18pt",
-      fontWeight: "bold",
+    backgroundColor: "#3A298F",
+    color: "#FAF6F6",
+  },
+  rightColumn: {
+    paddingTop: 30,
+    paddingRight: 15,
+    paddingLeft: 10,
+    "@media max-width: 400": {
+      width: "100%",
+      paddingRight: 0,
     },
-    text: {
-      fontSize: "14pt",
-      margin: "10px",
-      lineHeight: 1.6,
-    }
-  });
+    backgroundColor: "#FAF6F6",
+  },
+  title: {
+    fontSize: "18pt",
+    fontWeight: "bold",
+  },
+  text: {
+    fontSize: "14pt",
+    margin: "10px",
+    lineHeight: 1.6,
+  },
+})
 
-const getInfo = () => {
+const GetInfo = () => {
   const currentUser = useCurrentUser()
   const curriculumId = useParam("curriculumId", "number")
   const [curriculum] = useQuery(
@@ -73,15 +73,15 @@ const getInfo = () => {
       staleTime: Infinity,
     }
   )
-  const { role, userId,...user} = currentUser
+  const { role, userId, ...user } = currentUser
   const { name, ...info } = curriculum
-  return {... info, ...user}
+  return { ...info, ...user }
 }
 const PersonalInfo = (props) => {
-  const {info} = props
+  const { info } = props
   return (
     <View style={styles.text}>
-      <Text>{info.name+" "+info.lastName} </Text>
+      <Text>{info.name + " " + info.lastName} </Text>
       <Text>{info.profession}</Text>
       <Text>{info.description}</Text>
     </View>
@@ -91,36 +91,36 @@ const CurriculumDocument = (props) => {
   const info = props.curriculum
   const text = styles.text
   return (
-  <Document>
-    <Page size="A4">
-      <View style={styles.container}>
-        <View style={styles.leftColumn}> 
-          <PersonalInfo info={info}/>
-          <Skills skills={info.skills} styles={styles}/>
+    <Document>
+      <Page size="A4">
+        <View style={styles.container}>
+          <View style={styles.leftColumn}>
+            <PersonalInfo info={info} />
+            <Skills skills={info.skills} styles={styles} />
+          </View>
+          <View style={styles.rightColumn}>
+            <Experiences experiences={info.laboralExperiences} styles={{ text }} />
+            <AcademicEducations academicEducations={info.academicEducations} styles={{ text }} />
+            <TechnicalEducations technicalEducations={info.technicalEducations} styles={{ text }} />
+            <Publications publications={info.publications} styles={{ text }} />
+            <References references={info.references} styles={{ text }} />
+          </View>
         </View>
-        <View style={styles.rightColumn}>
-          <Experiences experiences={info.laboralExperiences} styles={{text}}/>
-          <AcademicEducations academicEducations={info.academicEducations} styles={{text}}/>
-          <TechnicalEducations technicalEducations={info.technicalEducations} styles={{text}}/>
-          <Publications publications={info.publications} styles={{text}}/>
-          <References references={info.references} styles={{text}}/>
-        </View>
-      </View>
-    </Page>
-  </Document>
+      </Page>
+    </Document>
   )
-};
+}
 
 const PDFViewPage = () => {
-    return (
-        <div style={{minHeight: "100vh"}}>
-            <Suspense fallback={<div>Loading...</div>}>
-              <PDFViewer style={{width: "100%", height:"90vh"}}>
-                <CurriculumDocument curriculum={getInfo()}/>
-              </PDFViewer>
-            </Suspense>
-        </div>
-    )
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <PDFViewer style={{ width: "100%", height: "90vh" }}>
+          <CurriculumDocument curriculum={GetInfo()} />
+        </PDFViewer>
+      </Suspense>
+    </div>
+  )
 }
 
 PDFViewPage.authenticate = true
