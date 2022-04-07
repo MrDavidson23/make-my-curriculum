9f738ae9281715b426f81fff559261b3dbb9e88d
Andrei Leon Salas <andreileonsalas@gmail.com>
2022-04-07 01:10:18 -0600

    changed version

--- blitz.config.js
+++ blitz.config.js
@@ -5,8 +5,14 @@ const withPWA = require("next-pwa")
 const runtimeCaching = require("next-pwa/cache")
 const config = withPWA({
   pwa: {
-    dest: "../../../public",
-
+    dest: "public",
+    fallbacks: {
+      image: "logoOnly.png",
+      // document: '/other-offline',  // if you want to fallback to a custom    page other than /_offline
+      // font: '/static/font/fallback.woff2',
+      // audio: ...,
+      // video: ...,
+    },
     runtimeCaching,
   },
   middleware: [
