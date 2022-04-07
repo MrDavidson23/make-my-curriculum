b665a5a27b1d21f557348c4a3e4df775163a5d0b
Torresmorah <Torresmorah@gmail.com>
2022-04-07 00:31:39 -0600

    Editable Title Component
    - Added EditableTitleText Component
    - Modified curriculum edit page and skill reading page to edit skill title

---
+++ app/core/components/EditableTitleText.js
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import { TextField, Typography } from "@mui/material"
+
+export const EditableTitleText = (props) => {
+    const { title, updateState, ...textProps } = props
+    const [text, setText] = useState(title)
+    return (
+        <Typography variant="h6" component="div" gutterBottom>
+            <TextField {...textProps} variant="standard"
+            label={title} 
+            value={text}
+            onChange={(event) => {
+                setText(event.target.value)
+                updateState(event)
+            }}/>
+        </Typography>
+    )
+}
+
+export default EditableTitleText
