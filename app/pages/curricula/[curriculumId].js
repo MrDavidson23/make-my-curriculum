50ec96c7af056f08bd4d37b59d585dddf2ced357
Sebas Quesada <58613474+MrDavidson23@users.noreply.github.com>
2022-04-07 18:43:24 -0600

    Changed view of curriculum

--- app/pages/curricula/[curriculumId].js
+++ app/pages/curricula/[curriculumId].js
@@ -1,65 +1,53 @@
 import { Suspense } from "react"
 import { Head, Link, useRouter, useQuery, useParam, useMutation, Routes } from "blitz"
+import { Grid, Button } from "@mui/material"
 import Layout from "app/core/layouts/Layout"
-import getCurriculum from "app/curricula/queries/getCurriculum"
-import deleteCurriculum from "app/curricula/mutations/deleteCurriculum"
-export const Curriculum = () => {
-  const router = useRouter()
-  const curriculumId = useParam("curriculumId", "number")
-  const [deleteCurriculumMutation] = useMutation(deleteCurriculum)
-  const [curriculum] = useQuery(getCurriculum, {
-    id: curriculumId,
-  })
-  return (
-    <>
-      <Head>
-        <title>Curriculum {curriculum.id}</title>
-      </Head>
-
-      <div>
-        <h1>Curriculum {curriculum.id}</h1>
-        <pre>{JSON.stringify(curriculum, null, 2)}</pre>
-
-        <Link
-          href={Routes.EditCurriculumPage({
-            curriculumId: curriculum.id,
-          })}
-        >
-          <a>Edit</a>
-        </Link>
-
-        <button
-          type="button"
-          onClick={async () => {
-            if (window.confirm("This will be deleted")) {
-              await deleteCurriculumMutation({
-                id: curriculum.id,
-              })
-              router.push(Routes.CurriculaPage())
-            }
-          }}
-          style={{
-            marginLeft: "0.5rem",
-          }}
-        >
-          Delete
-        </button>
-      </div>
-    </>
-  )
-}
+import PDFViewPage from "./[curriculumId]/pdf-view"
 
 const ShowCurriculumPage = () => {
+  const curriculumId = useParam("curriculumId", "number")
   return (
     <div>
-      <p>
-        <Link href={Routes.CurriculaPage()}>
-          <a>Curricula</a>
-        </Link>
-      </p>
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        mt={5}
+        mb={5}
+      >
+        <Grid item>
+          <Button variant="outlined">
+            <Link href={Routes.CurriculaPage()}>Ver Todos los Curriculums</Link>
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="outlined">
+            <Link href={Routes.EditCurriculumPage({ curriculumId: curriculumId })}>
+              Editar Curriculum
+            </Link>
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button
+            variant="outlined"
+            onClick={async () => {
+              console.log(curriculum.id)
+              if (window.confirm("This will be deleted")) {
+                await deleteCurriculumMutation({
+                  id: curriculum.id,
+                })
+                router.reload()
+              }
+            }}
+          >
+            Eliminar Curriculum
+          </Button>
+        </Grid>
+      </Grid>
 
       <Suspense fallback={<div>Loading...</div>}>
-        <Curriculum />
+        <PDFViewPage />
       </Suspense>
     </div>
   )
