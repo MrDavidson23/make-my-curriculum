b8d4984b16662d835be9d38ff506e060042f4b6f
Lucas <66132859+SilverSoul16@users.noreply.github.com>
2022-04-11 15:52:53 -0600

    Administrator user list

--- app/users/mutations/deleteUser.js
+++ app/users/mutations/deleteUser.js
@@ -1,10 +1,10 @@
 import { resolver } from "blitz"
 import db from "db"
 import { z } from "zod"
-const DeleteUser = z.object({
+const deleteUser = z.object({
   id: z.number(),
 })
-export default resolver.pipe(resolver.zod(DeleteUser), resolver.authorize(), async ({ id }) => {
+export default resolver.pipe(resolver.zod(deleteUser), resolver.authorize(), async ({ id }) => {
   // TODO: in multi-tenant app, you must add validation to ensure correct tenant
   const user = await db.user.deleteMany({
     where: {
