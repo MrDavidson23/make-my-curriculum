995d509b8f170321e33b3a5eddf2b76735805abd
Andrei Leon Salas <andreileonsalas@gmail.com>
2022-04-17 14:43:22 -0600

    implemented send funcionality

---
+++ integrations/mail.js
@@ -0,0 +1,26 @@
+const nodemailer = require("nodemailer")
+
+const smtpTransport = nodemailer.createTransport({
+  host: "ns2.plataformaelectronicacr.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: "noreply@makemycurriculum.plataformaelectronicacr.com",
+    pass: "QTkX64Bb&j0~",
+  },
+})
+
+module.exports = {
+  sendMail: async (req, res) => {
+    const mailOptions = req
+    smtpTransport.sendMail(mailOptions, (error, response) => {
+      if (error) {
+        console.log(error)
+        res.status(500).send("Error al enviar el correo")
+      } else {
+        console.log("Correo enviado")
+        res.status(200).send("Correo enviado")
+      }
+    })
+  },
+}
