4d18cfec6b1f9442c9e39a25e6cce82c9e597d73
Andrei Leon Salas <andreileonsalas@gmail.com>
2022-04-25 23:08:10 -0600

    Login as google

--- app/api/auth/[...auth].js
+++ app/api/auth/[...auth].js
@@ -2,7 +2,6 @@ import { passportAuth } from "blitz"
 import db from "db"
 import { Strategy as TwitterStrategy } from "passport-twitter"
 import { Strategy as GoogleStrategy } from "passport-google-oidc"
-import { Strategy as LinkedInStrategy } from "passport-linkedin-oauth2"
 
 export default passportAuth({
   successRedirectUrl: "/",
@@ -86,52 +85,6 @@ export default passportAuth({
             userId: user.id,
             roles: [user.role],
             source: "google",
-          }
-          done(undefined, { publicData })
-        }
-      ),
-    },
-    {
-      authenticateOptions: { scope: "r_emailaddress r_liteprofile" },
-      profileFields: ["id", "first-name", "last-name", "email-address", "headline"],
-      strategy: new LinkedInStrategy(
-        {
-          clientID: process.env.LINKEDIN_KEY,
-          clientSecret: process.env.LINKEDIN_SECRET,
-          //callbackURL: 'https://www.example.com/oauth2/redirect/google'
-          callbackURL:
-            process.env.NODE_ENV === "production"
-              ? "https://makemycurriculum.plataformaelectronicacr.com/api/auth/linkedin/callback"
-              : "http://localhost:3000/api/auth/linkedin/callback",
-          includeEmail: true,
-        },
-        async function (_token, _tokenSecret, profile, done) {
-          console.log("profile", profile)
-          console.log("_token", _token)
-          console.log("_tokenSecret", _tokenSecret)
-
-          const email = profile.emails && profile.emails[0]?.value
-          //const email = profile.emails && profile.emails[0]?.value
-
-          if (!email) {
-            // This can happen if you haven't enabled email access in your twitter app permissions
-            return done(new Error("LinkedIn OAuth response doesn't have email."))
-          }
-
-          const user = await db.user.upsert({
-            where: { email },
-            create: {
-              email,
-              name: profile.name.givenName,
-              lastName: profile.name.familyName,
-            },
-            update: { email },
-          })
-
-          const publicData = {
-            userId: user.id,
-            roles: [user.role],
-            source: "Linkedin",
           }
           done(undefined, { publicData })
         }
