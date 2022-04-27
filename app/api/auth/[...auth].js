c023c4f90ec7b1df84c24862ccbf226396c7183f
Andrei Leon Salas <andreileonsalas@gmail.com>
2022-04-26 19:26:13 -0600

    fix

--- app/api/auth/[...auth].js
+++ app/api/auth/[...auth].js
@@ -1,94 +1,84 @@
-import { passportAuth } from "blitz"
-import db from "db"
-import { Strategy as TwitterStrategy } from "passport-twitter"
-import { Strategy as GoogleStrategy } from "passport-google-oidc"
+import { AuthenticationError, Link, useMutation, Routes } from "blitz"
+import { LabeledTextField } from "app/core/components/LabeledTextField"
+import { Form, FORM_ERROR } from "app/core/components/Form"
+import login from "app/auth/mutations/login"
+import { Login } from "app/auth/validations"
+import { Grid } from "@mui/material"
+export const LoginForm = (props) => {
+  const [loginMutation] = useMutation(login)
+  return (
+    <div>
+      <Grid
+        container
+        direction="row"
+        spacing={2}
+        textAlign={"center"}
+        sx={{ mx: "auto", width: "100%" }}
+      >
+        <Grid item xs={12}>
+          <h1>Login</h1>
+          <a href="/api/auth/twitter">Log In With Twitter</a>
+          <p></p>
+          <a href="/api/auth/google">Log In With Google</a>
+          <p></p>
+          <a href="/api/auth/linkedin">Log In With LinkedIn</a>
+          <p></p>
+          <a href="/api/auth/facebook">Log In With Facebook</a>
+          <p></p>
+          <p>{process.env.NODE_ENV}</p>
+        </Grid>
+        <Grid item xs={12}>
+          <Form
+            submitText="Login"
+            schema={Login}
+            initialValues={{
+              email: "",
+              password: "",
+            }}
+            onSubmit={async (values) => {
+              try {
+                const user = await loginMutation(values)
+                props.onSuccess?.(user)
+              } catch (error) {
+                if (error instanceof AuthenticationError) {
+                  return {
+                    [FORM_ERROR]: "Sorry, those credentials are invalid",
+                  }
+                } else {
+                  return {
+                    [FORM_ERROR]:
+                      "Sorry, we had an unexpected error. Please try again. - " + error.toString(),
+                  }
+                }
+              }
+            }}
+          >
+            <LabeledTextField name="email" label="Email" placeholder="Email" />
+            <LabeledTextField
+              name="password"
+              label="Password"
+              placeholder="Password"
+              type="password"
+            />
 
-export default passportAuth({
-  successRedirectUrl: "/",
-  errorRedirectUrl: "/",
-  strategies: [
-    {
-      strategy: new TwitterStrategy(
-        {
-          consumerKey: process.env.TWITTER_CONSUMER_KEY,
-          consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-          callbackURL:
-            process.env.NODE_ENV === "production"
-              ? "https://makemycurriculum.plataformaelectronicacr.com/api/auth/twitter/callback"
-              : "http://localhost:3000/api/auth/twitter/callback",
-          includeEmail: true,
-        },
-        async function (_token, _tokenSecret, profile, done) {
-          console.log("profile", profile)
-          const email = profile.emails && profile.emails[0]?.value
-
-          if (!email) {
-            // This can happen if you haven't enabled email access in your twitter app permissions
-            return done(new Error("Twitter OAuth response doesn't have email."))
-          }
-
-          const user = await db.user.upsert({
-            where: { email },
-            create: {
-              email,
-              name: profile.displayName,
-            },
-            update: { email },
-          })
-
-          const publicData = {
-            userId: user.id,
-            roles: [user.role],
-            source: "twitter",
-          }
-          done(undefined, { publicData })
-        }
-      ),
-    },
-    {
-      authenticateOptions: { scope: "openid email profile" },
-      strategy: new GoogleStrategy(
-        {
-          clientID: process.env.GOOGLE_CLIENT_ID,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-          //callbackURL: 'https://www.example.com/oauth2/redirect/google'
-          callbackURL:
-            process.env.NODE_ENV === "production"
-              ? "https://makemycurriculum.plataformaelectronicacr.com/api/auth/google/callback"
-              : "http://localhost:3000/api/auth/google/callback",
-          includeEmail: true,
-        },
-        async function (_token, _tokenSecret, profile, done) {
-          console.log("profile", profile)
-          console.log("_token", _token)
-          console.log("_tokenSecret", _tokenSecret)
-
-          const email = _tokenSecret.emails[0].value || _tokenSecret.emails[0].value
-          //const email = profile.emails && profile.emails[0]?.value
-
-          if (!email) {
-            // This can happen if you haven't enabled email access in your twitter app permissions
-            return done(new Error("Google OAuth response doesn't have email."))
-          }
-
-          const user = await db.user.upsert({
-            where: { email },
-            create: {
-              email,
-              name: _tokenSecret.name.GivenName,
-              lastName: _tokenSecret.name.familyName,
-            },
-            update: { email },
-          })
-
-          const publicData = {
-            userId: user.id,
-            roles: [user.role],
-            source: "google",
-          }
-          done(undefined, { publicData })
-        }
-      ),
-    },
-  ],
-})
+            <div>
+              <Link href={Routes.ForgotPasswordPage()}>
+                <a>Forgot your password?</a>
+              </Link>
+            </div>
+          </Form>
+        </Grid>
+        <Grid item xs={12}>
+          <div
+            style={{
+              marginTop: "1rem",
+            }}
+          >
+            Or <Link href={Routes.SignupPage()}>Sign Up</Link>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+export default LoginForm
