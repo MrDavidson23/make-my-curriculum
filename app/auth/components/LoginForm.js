94f754648453aa87a804230f8bc0bdc72bac9cd9
Andrei Leon Salas <andreileonsalas@gmail.com>
2022-04-26 19:09:34 -0600

    facebook

--- app/auth/components/LoginForm.js
+++ app/auth/components/LoginForm.js
@@ -22,6 +22,10 @@ export const LoginForm = (props) => {
           <a href="/api/auth/google">Log In With Google</a>
           <p></p>
           <a href="/api/auth/linkedin">Log In With LinkedIn</a>
+          <p></p>
+          <a href="/api/auth/facebook">Log In With Facebook</a>
+          <p></p>
+          <p>{process.env.NODE_ENV}</p>
         </Grid>
 
         <Grid item xs={12}>
