1ca2602def6c0db666d0c3693afe45f04b778ba5
Andrei Leon Salas <andreileonsalas@gmail.com>
2022-04-26 18:01:27 -0600

    app.js to start manually

--- app/auth/components/LoginForm.js
+++ app/auth/components/LoginForm.js
@@ -23,8 +23,6 @@ export const LoginForm = (props) => {
           <p></p>
           <a href="/api/auth/linkedin">Log In With LinkedIn</a>
           <p></p>
-          <a href="/api/auth/facebook">Log In With Facebook</a>
-          <p></p>
           <p>{process.env.NODE_ENV}</p>
         </Grid>
 
