484f37dcfaff91eaea8224bf11a36286943c42c8
Andrei Leon Salas <andreileonsalas@gmail.com>
2022-04-28 10:14:10 -0600

    added seed and direction

--- db/migrations/20220422060741_delete_identification_type/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the `IdentificationType` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `IdentificationType`;
