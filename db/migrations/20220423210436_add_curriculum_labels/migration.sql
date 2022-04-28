d389643065aa3305e6a08f0780ff85ecba67d7a4
Andrei Leon Salas <andreileonsalas@gmail.com>
2022-04-28 10:14:10 -0600

    added seed and direction

--- db/migrations/20220423210436_add_curriculum_labels/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE `Curriculum` ADD COLUMN `academicEducationLabel` VARCHAR(191) NOT NULL DEFAULT 'Educación Académica',
-    ADD COLUMN `laboralExperienceLabel` VARCHAR(191) NOT NULL DEFAULT 'Experiencia Laboral',
-    ADD COLUMN `publicationLabel` VARCHAR(191) NOT NULL DEFAULT 'Publicaciones',
-    ADD COLUMN `referenceLabel` VARCHAR(191) NOT NULL DEFAULT 'Referencias',
-    ADD COLUMN `skillLabel` VARCHAR(191) NOT NULL DEFAULT 'Habilidades',
-    ADD COLUMN `technicalEducationLabel` VARCHAR(191) NOT NULL DEFAULT 'Educación Técnica';
---
+++ db/migrations/20220428160708_mc_13/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Language` ADD COLUMN `direction` VARCHAR(191) NULL;
