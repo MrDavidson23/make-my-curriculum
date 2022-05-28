d0be68262d94be441516982a01bc61b4ed719749
Torresmorah <Torresmorah@gmail.com>
2022-05-28 17:02:42 -0600

    Better layout on the new template page

--- app/templates/components/EditablePreview.js
+++ app/templates/components/EditablePreview.js
@@ -17,6 +17,36 @@ export const EditablePreview = (props) => {
         setName,
         ...previewProps} = props
 
+    const stylesState = {
+        left:  {state: props.leftStyles, set:setLeftStyles },
+        right: {state: props.rightStyles, set:setRightStyles }
+    }
+
+    const changeState = (sides,field,property,newValue) => {
+        sides.forEach((side) => {
+            const newState = JSON.parse(JSON.stringify(stylesState[side].state))
+            newState[field][property] = newValue
+            stylesState[side].set(newState)
+        })
+    }
+
+    const SelectFontWeight = ({value,label,onChange}) => {
+        return (
+            <Select
+                label={label}
+                value={value}
+                onChange={(event) => {onChange(event)}}
+            >
+                <MenuItem value="normal">
+                    Normal
+                </MenuItem>
+                <MenuItem value="bold">
+                    Bold
+                </MenuItem>
+            </Select>
+        )
+    }
+
     return (
     <>
     <Grid 
@@ -24,19 +54,19 @@ export const EditablePreview = (props) => {
         justifyContent="center" 
         alignItems="center">
 
-        <Typography variant="h6" component="div" gutterBottom color="secondary">
-        <Grid item xs={8}>
+    <Grid item xs={8}>
         <Grid container  
             justifyContent="space-between"
             alignItems="center"
             spacing={3}
         >
-            
-            <Grid item xs={12}>
+            <Grid container direction="row">
+            <Grid item xs={6}>
                 <TextField name="name" label="Nombre" placeholder="Nombre"
                 value={props.name}
                 onChange={(e)=>{ setName(e.target.value)}}/>
-                    Fuente
+            </Grid>
+            <Grid item xs={6}>
                 <Select
                     label={"Fuente"}
                     value={props.leftStyles.text.fontFamily}
@@ -58,19 +88,16 @@ export const EditablePreview = (props) => {
                     ))}
                 </Select>
             </Grid>
+            </Grid>
+            <Grid container direction="row">
             <Grid item xs={12}>
-            <Select
-                label={"Titulo"}
-                value={props.leftStyles.title.fontSize}
-                onChange={(event) => {
-                    const newLeftStyles = JSON.parse(JSON.stringify(props.leftStyles))
-                    const newRightStyles = JSON.parse(JSON.stringify(props.rightStyles))
-                    newLeftStyles.title.fontSize = event.target.value
-                    newRightStyles.title.fontSize = event.target.value
-                    setLeftStyles(newLeftStyles)
-                    setRightStyles(newRightStyles)
-                }}
-            >
+                <Select
+                    label={"Titulo"}
+                    value={props.leftStyles.title.fontSize}
+                    onChange={(event) => {
+                        changeState(["left","right"],"title","fontSize",event.target.value)
+                    }}
+                >
                 {fontSizesArray.map((item,i) => (
                     <MenuItem key={i} value={item}>
                     {item}
@@ -78,127 +105,108 @@ export const EditablePreview = (props) => {
                 ))}
             </Select>
             </Grid>
-            <Grid item xs={12}>
-            <Select
+            </Grid>
+            <Grid container direction="row">
+            <Grid item xs={3}>
+            <SelectFontWeight
                 label={"Titulo"}
                 value={props.leftStyles.title.fontWeight}
-                onChange={(event) => {
-                    const newStyles = JSON.parse(JSON.stringify(props.leftStyles))
-                    newStyles.title.fontWeight = event.target.value
-                    setLeftStyles(newStyles)
+                onChange={(event) => {                
+                    changeState(["left"],"title","fontWeight",event.target.value)
                 }}
-            >
-                <MenuItem value="normal">
-                    normal
-                </MenuItem>
-                <MenuItem value="bold">
-                    bold
-                </MenuItem>
-            </Select>
+            />
             </Grid>
-            <Grid item xs={12}>
-            <Select
-                label={"Titulo"}
-                value={props.rightStyles.title.fontWeight}
-                onChange={(event) => {
-                    const newStyles = JSON.parse(JSON.stringify(props.rightStyles))
-                    newStyles.title.fontWeight = event.target.value
-                    setRightStyles(newStyles)
-                }}
-            >
-                <MenuItem value="normal">
-                    normal
-                </MenuItem>
-                <MenuItem value="bold">
-                    bold
-                </MenuItem>
-            </Select>
-            </Grid>
-            <Grid item xs={6}>
-                    
-                    <Select
-                        label={"texto"}
-                        value={props.leftStyles.text.fontWeight}
-                        onChange={(event) => {
-                            const newStyles = JSON.parse(JSON.stringify(props.leftStyles))
-                            newStyles.text.fontWeight = event.target.value
-                            setLeftStyles(newStyles)
-                        }}
-                    >
-                        <MenuItem value="normal">
-                            normal
-                        </MenuItem>
-                        <MenuItem value="bold">
-                            bold
-                        </MenuItem>
-                    </Select>
-                    <Select
-                        label={"texto"}
-                        value={props.rightStyles.text.fontWeight}
-                        onChange={(event) => {
-                            const newStyles = JSON.parse(JSON.stringify(props.rightStyles))
-                            newStyles.text.fontWeight = event.target.value
-                            setRightStyles(newStyles)
-                        }}
-                    >
-                        <MenuItem value="normal">
-                            normal
-                        </MenuItem>
-                        <MenuItem value="bold">
-                            bold
-                        </MenuItem>
-                    </Select>
-                    <Select
-                        label={"Titulo"}
-                        value={props.leftStyles.text.fontSize}
-                        onChange={(event) => {
-                            const newLeftStyles = JSON.parse(JSON.stringify(props.leftStyles))
-                            const newRightStyles = JSON.parse(JSON.stringify(props.rightStyles))
-                            newLeftStyles.text.fontSize = event.target.value
-                            newRightStyles.text.fontSize = event.target.value
-                            setLeftStyles(newLeftStyles)
-                            setRightStyles(newRightStyles)
-                        }}
-                    >
-                        {fontSizesArray.map((item,i) => (
-                            <MenuItem key={i} value={item}>
-                            {item}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                    
-                </Grid>
-            <Grid item container direction="row">
-                <Grid item xs={6}>
-                <ColorPicker
-                    label='Color'
-                    color={props.leftStyles.text.color}
-                    onChangeComplete={(color)=>{
-                        const newStyles = JSON.parse(JSON.stringify(props.leftStyles))
-                        newStyles.text.color = color.hex
-                        setLeftStyles(newStyles)
-                    }}
-                />
-                </Grid>
-                <Grid item xs={6}>
+            <Grid item xs={3}>
                 <ColorPicker
                     label='Color'
                     color={props.leftStyles.title.color}
                     onChangeComplete={(color)=>{
-                        const newStyles = JSON.parse(JSON.stringify(props.leftStyles))
-                        newStyles.title.color = color.hex
-                        setLeftStyles(newStyles)
+                        changeState(["left"],"title","color",color.hex)
+                    }}
+                />
+            </Grid>
+            <Grid item xs={3}>
+            <SelectFontWeight
+                label={"Titulo"}
+                value={props.rightStyles.title.fontWeight}
+                onChange={(event) => {
+                    changeState(["right"],"title","fontWeight",event.target.value)
+                }}
+            />
+            </Grid>
+            <Grid item xs={3}>
+                <ColorPicker
+                    label='Color'
+                    color={props.rightStyles.title.color}
+                    onChangeComplete={(color)=>{
+                        changeState(["right"],"title","color",color.hex)
                     }}
                 />
                 </Grid>
+            </Grid>
+            <Grid container direction="row">
+                <Grid item xs={12}>
+                <Select
+                    label={"Titulo"}
+                    value={props.leftStyles.text.fontSize}
+                    onChange={(event) => {
+                        changeState(["left","right"],"text","fontSize",event.target.value)
+                    }}
+                >
+                    {fontSizesArray.map((item,i) => (
+                        <MenuItem key={i} value={item}>
+                        {item}
+                        </MenuItem>
+                    ))}
+                </Select>
+                </Grid>
+            </Grid>
+            <Grid container direction="row">
+                <Grid item xs={3}>
+                    <SelectFontWeight 
+                        label="texto"
+                        value={props.leftStyles.text.fontWeight}
+                        onChange={(event) => {
+                            changeState(["left"],"text","fontWeight",event.target.value)
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={3}>
+                <ColorPicker
+                    label='Color'
+                    color={props.leftStyles.text.color}
+                    onChangeComplete={(color)=>{
+                        changeState(["left"],"text","color",color.hex)
+                    }}
+                />
+                </Grid>
+                <Grid item xs={3}>
+                    <SelectFontWeight
+                        label={"texto"}
+                        value={props.rightStyles.text.fontWeight}
+                        onChange={(event) => {
+                            changeState(["right"],"text","fontWeight",event.target.value)
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={3}>
+                    <ColorPicker
+                        label='Color'
+                        color={props.rightStyles.text.color}
+                        onChangeComplete={(color)=>{
+                            changeState(["right"],"text","color",color.hex)
+                        }}
+                    />
+                </Grid>
+            </Grid>
+            
+            <Grid container direction="row">
                 <Grid item xs={6}>
                 <ColorPicker
                     label='Color'
                     color={props.leftStyles.container.backgroundColor}
                     onChangeComplete={(color)=>{
-                        const newStyles = JSON.parse(JSON.stringify(props.leftStyles))
-                        newStyles.container.backgroundColor = color.hex
-                        setLeftStyles(newStyles)
+                        changeState(["left"],"container","backgroundColor",color.hex)
                     }}
                 />
                 </Grid>
@@ -207,56 +215,31 @@ export const EditablePreview = (props) => {
                     label='Color'
                     color={props.rightStyles.container.backgroundColor}
                     onChangeComplete={(color)=>{
-                        const newStyles = JSON.parse(JSON.stringify(props.rightStyles))
-                        newStyles.container.backgroundColor = color.hex
-                        setRightStyles(newStyles)
-                    }}
-                />
-                </Grid>
-                <Grid item xs={6}>
-                <ColorPicker
-                    label='Color'
-                    color={props.rightStyles.text.color}
-                    onChangeComplete={(color)=>{
-                        const newStyles = JSON.parse(JSON.stringify(props.rightStyles))
-                        newStyles.text.color = color.hex
-                        setRightStyles(newStyles)
-                    }}
-                />
-                </Grid>
-                <Grid item xs={6}>
-                <ColorPicker
-                    label='Color'
-                    color={props.rightStyles.title.color}
-                    onChangeComplete={(color)=>{
-                        const newStyles = JSON.parse(JSON.stringify(props.rightStyles))
-                        newStyles.title.color = color.hex
-                        setRightStyles(newStyles)
+                        changeState(["right"],"container","backgroundColor",color.hex)
                     }}
                 />
                 </Grid>
             </Grid>
-            <Grid item xs={8}>
-                <Typography variant="h6" component="div" gutterBottom> Distribucción </Typography>
-                <Slider
-                defaultValue={defaultValue}
-                getAriaValueText={(value) => value+"%"}
-                valueLabelDisplay="auto"
-                step={10}
-                marks
-                min={10}
-                max={90}
-                onChange={(e)=>{
-                    setPercentage(e.target.value)
-                    const newStyles = JSON.parse(JSON.stringify(props.leftStyles))
-                    newStyles.container.width = 600*(e.target.value/100)
-                    setLeftStyles(newStyles)
-                }}
-                />
+            <Grid container direction="row" justifyContent="center" >
+                <Grid item xs={8}>
+                    <Typography variant="h6" component="div" gutterBottom> Distribucción </Typography>
+                    <Slider
+                    defaultValue={defaultValue}
+                    getAriaValueText={(value) => value+"%"}
+                    valueLabelDisplay="auto"
+                    step={10}
+                    marks
+                    min={10}
+                    max={90}
+                    onChange={(e)=>{
+                        setPercentage(e.target.value)
+                        changeState(["left"],"container","width",600*(e.target.value/100))
+                    }}
+                    />
+                </Grid>
+            </Grid>
             </Grid>
         </Grid>
-        </Grid>
-        </Typography>
 
         <Grid item>
             <Preview 
