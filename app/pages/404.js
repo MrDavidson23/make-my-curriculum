380919c15cd7c9059da6fa17da43d700050fe4f5
Lucas <66132859+SilverSoul16@users.noreply.github.com>
2022-06-06 15:02:23 -0600

    404 page 5 seconds timer, redirects to Home

--- app/pages/404.js
+++ app/pages/404.js
@@ -1,10 +1,21 @@
-import { Head, ErrorComponent } from "blitz" // ------------------------------------------------------
+import { useRouter, Routes, Head, ErrorComponent } from "blitz"
+import { useEffect } from "react" // ------------------------------------------------------
 // This page is rendered if a route match is not found
 // ------------------------------------------------------
 
-export default function Page404() {
+const Page404 = () => {
+  const router = useRouter()
   const statusCode = 404
-  const title = "This page could not be found"
+  const title = "No se pudo encontrar esta página. Redirigiendo a inicio en 5 segundos"
+
+  // Timer 5 seconds to redirect to Home
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push(Routes.Home())
+    }, 5000)
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <>
       <Head>
@@ -16,3 +27,5 @@ export default function Page404() {
     </>
   )
 }
+
+export default Page404
